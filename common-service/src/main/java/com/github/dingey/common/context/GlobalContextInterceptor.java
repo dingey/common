dda478e7d4390332929e6920541c3d21a5c7861c
d <d>
2021-02-26 21:41:29 +0800

    a

--- common-service/src/main/java/com/github/dingey/common/context/GlobalContextInterceptor.java
+++ common-service/src/main/java/com/github/dingey/common/context/GlobalContextInterceptor.java
@@ -1,6 +1,5 @@
 package com.github.dingey.common.context;
 
-import com.github.dingey.common.util.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
@@ -8,7 +7,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
 
 public class GlobalContextInterceptor implements HandlerInterceptor {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -18,9 +16,7 @@ public class GlobalContextInterceptor implements HandlerInterceptor {
         String string = request.getHeader(GlobalContext.HEADER_NAME);
         if (StringUtils.hasText(string)) {
             log.debug("拦截到全局上下文标识{}，值为:{}", GlobalContext.HEADER_NAME, string);
-            @SuppressWarnings("unchecked")
-            HashMap<String, String> map = JsonUtil.parseJson(string, HashMap.class);
-            GlobalContext.setContextMap(map);
+            GlobalContext.setByJsonString(string);
         }
         return true;
     }
